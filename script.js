43f9fc6729fdbc23e032129eab4067762a8bc37f
fiko942 <tobellord@gmail.com>
2022-06-25 17:28:41 +0700

    Adding the download button

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 window.onload = () => {
-    const developer = {
+    const d = {
         name: 'Wiji Fiko Teren'
     };
     if(images !== undefined) {
         const imageContainer = document.querySelector('.imgs');
         images.forEach(image => {
-            let i = document.createElement('img');
-            i.setAttribute('src', image);
-            i.setAttribute('alt', developer.name);
-            i.setAttribute('title', developer.name);
-            i.classList.add('galeri-footer-image');
-            imageContainer.appendChild(i);
+            let element = document.createElement('img');
+            element.setAttribute('src', image);
+            element.setAttribute('alt', d.name);
+            element.setAttribute('title', d.name);
+            element.classList.add('galeri-footer-image');
+            imageContainer.appendChild(element);
         });
     }
+
+
+    document.querySelectorAll('button[href]').forEach(btn => {
+        btn.addEventListener('click', () => {
+            window.open(btn.getAttribute('href'), '_blank');
+        });
+    });
 }
